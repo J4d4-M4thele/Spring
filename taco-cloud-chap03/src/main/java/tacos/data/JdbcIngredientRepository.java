dfa31ef1ef5d54594675efdb375115832774ebe5
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-03-13 09:46:15 +0200

    update

---
+++ taco-cloud-chap03/src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -0,0 +1,11 @@
+package tacos.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import tacos.Ingredient;
+
+public class JdbcIngredientRepository implements IngredientRepository{
+
+}
