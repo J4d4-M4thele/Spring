4b3fe8e688dce587c0ec0892d888114dfcdad57c
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-03-13 16:24:45 +0200

    update

--- taco-cloud-chap05/src/main/java/tacos/data/IngredientRepository.java
+++ taco-cloud-chap05/src/main/java/tacos/data/IngredientRepository.java
@@ -1,5 +1,10 @@
 package tacos.data;
 
-public interface IngredientRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import tacos.Ingredient;
+
+public interface IngredientRepository 
+         extends CrudRepository<Ingredient, String> {
 
 }
